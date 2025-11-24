a382ce5907814832118820217e44b6c6f461830a
thang <thang@gmail.com>
2025-11-25 00:38:12 +0700

    feature: Create UI product-detail, payment, cart

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -4,7 +4,11 @@ import viteLogo from '/vite.svg'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/HeaderComponent/header.tsx';
+import Footer from './components/FooterComponent/footer.tsx';
 import LoginPage from './pages/Login/LoginPage.tsx';
+import ProductDetail from './pages/DetailProduct/DetailProductPage.tsx';
+import Cart from './pages/Cart/Cart.tsx';
+import Checkout from './pages/Payment/Checkout.tsx';
 import './Global.css'
 
 
@@ -13,8 +17,12 @@ function App() {
     <Router>
       <Header />
       <Routes>
+        <Route path="/product-detail" element={<ProductDetail />} />
+        <Route path="/check-out" element={<Checkout />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
