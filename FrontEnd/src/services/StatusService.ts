cbe903ffb0c35ea377e858b6c583d17a69ef1907
Quang Huy <tanquanghuy.2302@gmail.com>
2026-01-13 17:45:54 +0700

    feat: order_detail

--- FrontEnd/src/services/StatusService.ts
+++ FrontEnd/src/services/StatusService.ts
@@ -1,6 +1,8 @@
 import axiosClient from "./AxiosClient";
 import type { OrderResponse, OrderWithUserResponse } from "./Interface";
 
+export type OrderStatus = "PENDING" | "APPROVED" | "CANCELLED";
+
 const OrderService = {
   getById: (id: number): Promise<OrderResponse> =>
     axiosClient.get(`/api/order/${id}`),
@@ -9,7 +11,7 @@ const OrderService = {
     const res = await axiosClient.get("/api/order/list/with-user");
     return res.data;
   },
-  updateStatus: (id: number, status: "APPROVED" | "CANCELLED") =>
+  updateStatus: (id: number, status: OrderStatus) =>
     axiosClient.put(`/api/order/${id}`, { status }),
 };
 
